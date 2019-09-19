5a5636628a37940ca79b167996d4d3a73e5ee516
wahidaleena <34679809+wahidaleena@users.noreply.github.com>
2019-09-19 09:52:20 +0530

    Update include.cbl

--- include.cbl
+++ include.cbl
@@ -1,1 +1,26 @@
+IDENTIFICATION DIVISION.
+       PROGRAM-ID.    TEST DB2.
+      *SECURITY.      OPERACTION, REVISION, AND DISTRIBUTION
+      *            OF THIS PROGRAM BY WRITTEN AUTHORIZATION
+      *            OF THE ABOVE INSTALLACTION ONLY.
+      *DATE-WRITTEN.  09/12/19.
+      *DATE-COMPLETED.
+      **************************CC109**********************************
+       
+       PROCEDURE DIVISION.
+       0000-INITIALIZE-PARA.
+          
+        EXEC SQL INCLUDE SQLDA;
+
+        EXEC SQL DECLARE C1 CURSOR FOR
+          SELECT DEPTNO, DEPTNAME, MGRNO FROM TDEPT
+          WHERE ADMRDEPT = 'A00';
+          END-EXEC.
+     
+       6000-FINAL-COUNT.
+          CLOSE INFILE
+                OUT.
+          DISPLAY "-----------------------------------------------------".
+
+
 
